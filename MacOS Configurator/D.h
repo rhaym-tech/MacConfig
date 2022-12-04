f74f3cced73a2ee6c37aadfa796ae0360069f31e
rhaym-tech <rhaymtech2004@gmail.com>
2022-12-04 09:21:24 -0800

    Initial Commit

---
+++ MacOS Configurator/D.h
@@ -0,0 +1,6 @@
+#ifndef MacOS_Configurator_Bridging_Header_h
+#define MacOS_Configurator_Bridging_Header_h
+#import <CommonCrypto/CommonDigest.h>
+
+
+#endif /* MacOS_Configurator_Bridging_Header_h */
---
+++ MacOS Configurator/MacOS-Configurator-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MacOS-Configurator-Bridging-Header.h
+//  MacOS Configurator
+//
+//  Created by Rhaym Ghellab on 28/11/2022.
+//  Copyright © 2022 notiflux. All rights reserved.
+//
+
+#ifndef MacOS_Configurator_Bridging_Header_h
+#define MacOS_Configurator_Bridging_Header_h
+
+
+#endif /* MacOS_Configurator_Bridging_Header_h */
